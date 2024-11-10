36e29146420d56514ed13b07f34cb27f8b27617e
murilo <muriloalencs@gmail.com>
2024-11-10 12:42:41 -0400

    ajustes

--- backend/start-backend.sh
+++ backend/start-backend.sh
@@ -3,10 +3,10 @@
 # Define environment variable
 export FLASK_APP="run.py"
 export FLASK_DB_TYPE="postgres"
-export FLASK_DB_USER="postgres"
-export FLASK_DB_NAME="postgres"
-export FLASK_DB_PASSWORD="secretpass"
-export FLASK_DB_HOST="localhost"
+export FLASK_DB_USER="myuser"
+export FLASK_DB_NAME="mydb"
+export FLASK_DB_PASSWORD="mypassword"
+export FLASK_DB_HOST="postgres"
 export FLASK_DB_PORT="5432"
 
 # Run app.py when the container launches
